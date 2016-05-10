9656b6846ae084b09c24ab0b844f355a18422f88
Alexey Berdnikov <aberdnikov@gmail.com>
2016-05-10 10:27:48 +0500

    SPA -> Boot

--- src/init.php
+++ src/init.php
@@ -1,3 +1,3 @@
 <?php
 //регистрируем провайдеры
-Larakit\SPA::register_provider(\Larakit\Cmdbackup\LarakitServiceProvider::class);
+Larakit\Boot::register_provider(\Larakit\Cmdbackup\LarakitServiceProvider::class);
